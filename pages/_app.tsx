693ead4f3aea0dd1d6c7b767ddef7ee6976644f5
Michael Downs <michaeldowns@Mac.lan>
2025-06-15 21:57:25 -0400

    Update website title to 'Nano Neurosciences | Revolutionizing Vision'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/fonts.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Nano Neurosciences | Revolutionizing Vision</title>
+        <meta name="description" content="Nano Neurosciences - Advancing vision technology through cutting-edge research" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
